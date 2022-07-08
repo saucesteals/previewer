4ccfba37470ac58281d7797c3560f73619a071c5
Daniel <saucesteals@gmail.com>
2022-07-08 12:00:57 -0700

    activity

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,53 @@ use serenity::{
     async_trait,
     client::{Context, EventHandler},
     json::{self, Value},
-    model::{channel::Message, gateway::Ready},
+    model::{
+        channel::Message,
+        gateway::{Activity, Ready},
+        guild::Guild,
+    },
     prelude::GatewayIntents,
     Client,
 };
-use std::env;
+use std::{
+    env,
+    sync::atomic::{AtomicUsize, Ordering},
+};
 
 struct Handler<'a> {
     providers: [&'a dyn Provider; 1],
+    guild_count: AtomicUsize,
 }
 
 impl Default for Handler<'_> {
     fn default() -> Self {
         Self {
             providers: [&TiktokProvider {}],
+            guild_count: AtomicUsize::new(0),
         }
     }
 }
 
 #[async_trait]
 impl EventHandler for Handler<'_> {
-    async fn ready(&self, _ctx: Context, about: Ready) {
-        println!("Ready as {}", about.user.name);
+    async fn guild_create(&self, ctx: Context, _guild: Guild) {
+        let guild_count = self.guild_count.fetch_add(1, Ordering::SeqCst);
+
+        if guild_count % 10 == 0 {
+            ctx.set_activity(Activity::watching(format!("{} guilds", guild_count)))
+                .await;
+        }
     }
+
+    async fn ready(&self, ctx: Context, about: Ready) {
+        println!("Ready as {}", about.user.name);
+
+        let guild_count = about.guilds.len();
+        self.guild_count.store(guild_count, Ordering::SeqCst);
+        ctx.set_activity(Activity::watching(format!("{} guilds", guild_count)))
+            .await;
+    }
+
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.author.bot {
             return;
